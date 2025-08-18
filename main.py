28fcd353d5818a82c4d513015452f8ebbd95ae31
mabuhurairah <mabuhurairah07@gmail.com>
2025-08-18 07:20:41 +0000

    updated main.py

--- main.py
+++ main.py
@@ -2,29 +2,52 @@ from flask import Flask, jsonify, request
 import runpod
 from dotenv import load_dotenv
 import os
+import uuid
 
 load_dotenv()
 
 app = Flask(__name__)
 
 runpod.api_key = os.environ.get("RUNPOD_API_KEY")
+print(os.environ.get("RUNPOD_API_KEY"))
 
 
 @app.route("/start_server", methods=["GET"])
 def start_server():
-    pod = runpod.create_pod(
-        name="test_pod",
-        image_name="ubuntu:latest",
-        gpu_type_id="NVIDIA A40",
-        gpu_count=1,
-        container_disk_in_gb=40,
-        volume_in_gb=10,
-        cloud_type="SECURE",
-        ports="5000/http,2121/tcp",
-        docker_args="sleep infinity",
-    )
-    pod_id = pod["id"]
-    return jsonify(pod_id), 200
+    try:
+        pod_name = str(uuid.uuid4())
+        pod = runpod.create_pod(
+            name=pod_name,
+            image_name="hurairahhere/flux-lora-trainer:latest",  # Your Docker container
+            gpu_type_id="NVIDIA A40",
+            gpu_count=1,
+            container_disk_in_gb=20,
+            volume_in_gb=70,
+            volume_mount_path="/workspace",
+            cloud_type="SECURE",
+            ports="5000/http,2121/tcp",
+            env={
+                # Add any environment variables your container needs
+                "WORKSPACE_DIR": "/workspace",
+                "PYTHONPATH": "/workspace",
+            }
+        )
+        pod_id = pod["id"]
+        
+        return jsonify({
+            "status": True,
+            "response": pod_id,
+            "message": "Pod created successfully",
+            "status_code": 200
+        }), 200
+        
+    except Exception as e:
+        return jsonify({
+            "status": False,
+            "message": str(e),
+            "response" : None,
+            "status_code" : 500
+        }), 500
 
 
 if __name__ == "__main__":
