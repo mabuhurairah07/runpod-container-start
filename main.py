fdaf8b7e5b127e2f629be27666f7f9e351b3bce9
mabuhurairah <mabuhurairah07@gmail.com>
2025-08-31 13:20:49 +0300

    added image genartion api

--- main.py
+++ main.py
@@ -30,24 +30,75 @@ def start_server():
                 # Add any environment variables your container needs
                 "WORKSPACE_DIR": "/workspace",
                 "PYTHONPATH": "/workspace",
-            }
+            },
         )
         pod_id = pod["id"]
-        
-        return jsonify({
-            "status": True,
-            "response": pod_id,
-            "message": "Pod created successfully",
-            "status_code": 200
-        }), 200
-        
+
+        return (
+            jsonify(
+                {
+                    "status": True,
+                    "response": pod_id,
+                    "message": "Pod created successfully",
+                    "status_code": 200,
+                }
+            ),
+            200,
+        )
+
     except Exception as e:
-        return jsonify({
-            "status": False,
-            "message": str(e),
-            "response" : None,
-            "status_code" : 500
-        }), 500
+        return (
+            jsonify(
+                {
+                    "status": False,
+                    "message": str(e),
+                    "response": None,
+                    "status_code": 500,
+                }
+            ),
+            500,
+        )
+
+
+@app.route("/start_generation_server", methods=["GET"])
+def start_generation_server():
+    try:
+        pod_name = f"{str(uuid.uuid4())}_image_generation"
+        pod = runpod.create_pod(
+            name=pod_name,
+            image_name="hurairahhere/flux-image-generation",
+            gpu_type_id="NVIDIA A40",
+            gpu_count=1,
+            container_disk_in_gb=55,
+            cloud_type="SECURE",
+            ports="5000/http,2121/tcp",
+        )
+        pod_id = pod["id"]
+
+        return (
+            jsonify(
+                {
+                    "status": True,
+                    "response": pod_id,
+                    "message": "Pod created successfully",
+                    "status_code": 200,
+                }
+            ),
+            200,
+        )
+
+    except Exception as e:
+        return (
+            jsonify(
+                {
+                    "status": False,
+                    "message": str(e),
+                    "response": None,
+                    "status_code": 500,
+                }
+            ),
+            500,
+        )
 
 
 if __name__ == "__main__":
